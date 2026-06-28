f6831f80fb3b27fad4355ecc08557c8f56e1f79e
spuddu <stefano.puddu@cbsm.it>
2026-06-29 00:57:36 +0200

    Aggiorna pubblicazione gestionale2

--- docs/cdn/40/widgets/layout/column/dist/runtime/translations/it.js
+++
@@ -1,1 +0,0 @@
-System.register([],function(e){return{execute:function(){e({_widgetLabel:"Colonna",widgetProperties:"Propriet\xe0 del widget",widgetFunctions:"Funzioni del widget",widgetName:"nome del widget:",widgetProps:"propriet\xe0 del widget:",tips:"Colonna"})}}});
--- docs/cdn/40/widgets/layout/row/dist/runtime/translations/it.js
+++
@@ -1,1 +0,0 @@
-System.register([],function(e){return{execute:function(){e({_widgetLabel:"Riga",widgetProperties:"Propriet\xe0 del widget",widgetFunctions:"Funzioni del widget",widgetName:"nome del widget:",widgetProps:"propriet\xe0 del widget:",tips:"Riga"})}}});
